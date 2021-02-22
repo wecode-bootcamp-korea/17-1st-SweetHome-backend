cc8da2d7fed8de68a8b8af983f63c981e67de518
namujinju <namujinju@gmail.com>
2021-02-22 20:58:42 +0900

    Modify: order,posting 앱 models.py 수정 - 주문 상태를 나타내는 OrderStatus 모델 추가 - PostingComment 모델 posting 필드에 related_name 추가

--- order/models.py
+++ order/models.py
@@ -4,7 +4,6 @@ from user.models    import User
 from product.models import ProductOption
 
 class Order(models.Model):
-    user                   = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True)
     created_at             = models.DateTimeField(auto_now_add=True)
     sender_name            = models.CharField(max_length=45)
     sender_email           = models.CharField(max_length=45)
@@ -12,15 +11,25 @@ class Order(models.Model):
     recipient_name         = models.CharField(max_length=45)
     recipient_phone_number = models.CharField(max_length=45)
     recipient_address      = models.CharField(max_length=255)
-    total_amount           = models.DecimalField(decimal_places=2, max_digits=12)
+    total_price            = models.DecimalField(decimal_places=2, max_digits=12)
 
     class Meta:
-        db_table= 'orders'
+        db_table = 'orders'
+
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'order_statuses'
 
 class OrderProduct(models.Model):
+    user           = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True)
     product_option = models.ForeignKey('product.ProductOption', on_delete=models.SET_NULL, null=True)
     quantity       = models.IntegerField()
-    order          = models.ForeignKey('Order', on_delete=models.CASCADE)
+    created_at     = models.DateTimeField(auto_now_add=True)
+    updated_at     = models.DateTimeField(auto_now=True)
+    status         = models.ForeignKey('OrderStatus', on_delete=models.SET_NULL, null=True)
+    order          = models.ForeignKey('Order', on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'order_products'
