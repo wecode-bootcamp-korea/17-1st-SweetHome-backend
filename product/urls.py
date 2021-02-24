555f5850ce8b9a475bd1b474b1843c7d2932f9a2
sua cho <devsu7477@gmail.com>
2021-02-24 15:43:20 +0900

    ADD: ProductDetailView 작성 - 상품 상세페이지 views.py, urls.py 작성

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ProductView, ProductReviewView
+from .views import ProductView, ProductDetailView, ProductReviewView
 
 urlpatterns = [
     path('', ProductView.as_view()),
+    path('/<int:product_id>', ProductDetailView.as_view()),
     path('/<int:product_id>/review', ProductReviewView.as_view()),
 ]
