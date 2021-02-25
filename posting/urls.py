d83c7de83e7bf06d681da6a0a8bd306416de78b5
이준하 <ijunha@ijunhaui-MacBookPro.local>
2021-02-25 11:20:18 +0900

    유저 view.py signin 작성 완료

--- posting/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views      import PostingScrapView
-
-
-urlpatterns = [
-    path('/scrap', PostingScrapView.as_view()),
-]
--- posting/views.py
+++ posting/views.py
@@ -1,23 +1,3 @@
-import json
+from django.shortcuts import render
 
-from django.views import View
-from django.http  import JsonResponse
-
-from user.utils  import login_decorator
-from .models import PostingScrap
-
-
-class PostingScrapView(View):
-    @login_decorator
-    def post(self, request):
-        data       = json.loads(request.body)
-        user_id    = request.user
-        posting_id = data['posting_id']
-
-        if PostingScrap.objects.filter(user=user_id, posting_id=posting_id).exists():
-            posting_scrap = PostingScrap.objects.filter(user=user_id, posting_id=posting_id)
-            posting_scrap.delete()
-            return JsonResponse({'message' : 'SUCCESS'}, status=204)
-
-        PostingScrap.objects.create(user=user_id, posting_id=posting_id)
-        return JsonResponse({'message' : 'SUCCESS'}, status=201)
+# Create your views here.
