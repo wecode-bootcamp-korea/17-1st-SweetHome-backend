1f9fb876046301eb1f472ce73d0e50d7bc58e224
JELLO-Kim <cogus950403@gmail.com>
2021-02-25 20:44:28 +0900

    Modify : 비회원에게 None 부여 - 토큰이 있을 경우 회원 반환 - 토큰이 없을 경우 error 대신 None 반환

---
+++ user/utils.py
@@ -0,0 +1,24 @@
+import jwt
+import json
+
+from django.http    import JsonResponse, HttpResponse
+
+from my_settings    import SECRET_KEY, ALGORITHM
+from user.models    import User
+
+def login_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token    = request.headers.get('Authorization', None)
+            if not access_token:
+                request.user = None
+                return func(self, request, *args, **kwargs)
+            payload         = jwt.decode(access_token, SECRET_KEY, algorithms=ALGORITHM)
+            login_user      = User.objects.get(id=payload['user_id'])
+            request.user    = login_user
+            return func(self, request, *args, **kwargs)
+        except jwt.DecodeError:
+            return JsonResponse({'message' : 'INVALID_TOKEN'}, status=400)
+        except User.DoesNotExist:
+            return JsonResponse({'message' : 'INVALID_USER'}, status=401)
+    return wrapper
