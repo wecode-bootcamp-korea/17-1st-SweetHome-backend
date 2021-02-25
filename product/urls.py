8efac7cbcd6e475d5952b0034b05602863baae8b
sua cho <devsu7477@gmail.com>
2021-02-25 10:29:22 +0900

    ADD: CategoryView 작성 - 카테고리 정보 views.py, urls.py 작성

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ProductView, ProductReviewView
+from .views import ProductView, ProductReviewView, CategoryView
 
 urlpatterns = [
     path('', ProductView.as_view()),
     path('/<int:product_id>/review', ProductReviewView.as_view()),
-]
+    path('/category', CategoryView.as_view()),
+    ]
