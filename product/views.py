05d144268a18cc794d0a4e7f0c29b20b03f7f531
namujinju <namujinju@gmail.com>
2021-02-25 14:09:09 +0900

    Modify: 장바구니에 상품 담기 추가 - 상품 상세 페이지에서 정보를 받아 DB에 저장

--- product/views.py
+++ product/views.py
@@ -7,8 +7,12 @@ from django.db.models import Avg, Count, Q
 from product.models import (
   Product, 
   ProductReview, 
-  ReviewLike
+  ReviewLike,
+  ProductOption,
+  ProductColor,
+  ProductSize,
 )
+from order.models   import OrderProduct, Order, OrderStatus
 
 DISCOUNT_PROUDCTS_COUNT = 5
 
@@ -66,6 +70,45 @@ class ProductView(View):
         products_count = products.count()
 
         return JsonResponse({'products' : products_list, 'count' : products_count}, status=200)
+
+class ProductDetailView(View):
+    @login_decorator
+    def post(self, request, product_id):
+        try:
+            if not Product.objects.filter(id=product_id).exists():
+                return JsonResponse({'message':'INVALID_PRODUCT'}, status=404)
+
+            product = Product.objects.get(id=product_id)
+            
+            data     = json.loads(request.body)
+            color    = ProductColor.objects.get(name=data['color'])
+            size     = ProductSize.objects.get(name=data['size'])
+            quantity = int(data['quantity'])
+
+            user = request.user
+
+            product_option = ProductOption.objects.update_or_create(
+                product=Product.objects.get(id=product_id),color=color, size=size
+            )[0]
+            order = Order.objects.update_or_create(user=user, status=OrderStatus.objects.get(id=1))[0]
+
+            if OrderProduct.objects.filter(order=order, product_option=product_option, order__status=1).exists(): 
+                order_products = OrderProduct.objects.filter(order=order, product_option=product_option)
+                for order_product in order_products:
+                    order_product.quantity+=quantity
+                    order_product.save()
+            else:
+                OrderProduct.objects.create(
+                    order=order, product_option=product_option, quantity=quantity
+                )
+
+            return JsonResponse({'message':'SUCCESS'}, status=200)
+
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({'message':'JSON_DECODE_ERROR'}, status=400)
+        
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
 
 class ProductReviewView(View):
     def get(self, request, product_id):
